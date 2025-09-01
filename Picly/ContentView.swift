545e45713cd439162607b5ceed557da220b91033
zuss4dee <dami4adeosun@icloud.com>
2025-09-01 23:44:23 +0100

    Initial Commit

---
+++ Picly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Picly
+//
+//  Created by Damilare Adeosun on 01/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Picly/PiclyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiclyApp.swift
+//  Picly
+//
+//  Created by Damilare Adeosun on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PiclyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
